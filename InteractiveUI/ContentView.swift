150e017352e3da12ec08885275db4ac9a947c58a
Hannah Denaer <denaerhannah@gmail.com>
2025-04-19 15:31:48 -0500

    Added alert that displays to user when they click submit

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,24 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var name = ""
-    @State private var textTitle = "What is your name?"
+    @State private var date = ""
+    @State private var presentAlert = false
     var body: some View {
         VStack {
-            Text(textTitle)
+            Text("Tell us about yourself!")
                 .font(.title)
             TextField("Type your name here", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
-            Button("Submit Name") {
-                textTitle = "Welcome, \(name)!"
-                name = ""
+            TextField("Type your birthdate here", text: $date)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            Button("Submit") {
+                presentAlert = true
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
         }
-        .padding()
+        .alert("Thank you \(name), we know your birthday is \(date)!", isPresented: $presentAlert, actions: {
+        })
     }
 }
 
